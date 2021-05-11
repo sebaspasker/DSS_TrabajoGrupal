3c1b98b2ed66d7583e9de441e04133368af6fe1e
Raul Siguenza <rss75@alu.ua.es>
2021-05-11 13:21:41 +0200

    sanear slugname

--- NewsMoment/app/Http/Controllers/CategoryController.php
+++ NewsMoment/app/Http/Controllers/CategoryController.php
@@ -63,11 +63,49 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
-        $category=Category::find($id);
+    public function edit($id){
+        $category = Category::find($id);
         return view('category.edit')->with('categories', $category);
     }
+
+
+
+
+function sanear_string($string){
+    $string = trim($string); 
+    $string = str_replace(
+        array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+        array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+        $string
+    );
+    $string = str_replace(
+        array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+        array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+        $string
+    );
+    $string = str_replace(
+        array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+        array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+        $string
+    );
+    $string = str_replace(
+        array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+        array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+        $string
+    );
+    $string = str_replace(
+        array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+        array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+        $string
+    );
+    $string = str_replace(
+        array('ñ', 'Ñ', 'ç', 'Ç'),
+        array('n', 'N', 'c', 'C',),
+        $string
+    ); 
+    return $string;
+}
+
 
     /**
      * Update the specified resource in storage.
@@ -76,15 +114,12 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        $category=Category::find($id);
-
-        $category = new Category();
-        $category->name=$request->get('name');
-        $category->slugname=$request->get('slugname');
-        $category->image=$request->get('image');
-
+    public function update(Request $request, $id){
+        $category = Category::find($id);
+        // $category = new Category();
+        $category->name = $request->get('name');
+        $category->slugname = $request->get('sanear_string(name)');
+        $category->image = $request->get('image');
         $category->save();
         return redirect('/category');
     }
